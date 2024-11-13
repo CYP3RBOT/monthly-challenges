5f53ace1302c0e46c8933026b607da0fc2206f0c
CYP3RBOT <cyp3rbot8009@gmail.com>
2024-11-13 07:49:11 -0700

    Add febuary url

--- challenges/views.py
+++ challenges/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
 
-def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello world!")
+def january(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Do 10 pushups every day!")
+
+def febuary(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Walk for at least 20 minutes every day!")
