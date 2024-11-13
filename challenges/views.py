d5fbf259a0d010bb0e90545d9622332aca764ddd
CYP3RBOT <cyp3rbot8009@gmail.com>
2024-11-13 14:03:46 -0700

    Dynamic rendering tags

--- challenges/views.py
+++ challenges/views.py
@@ -20,17 +20,12 @@ MONTHLY_CHALLENGES = {
 # Create your views here.
 
 def index(request: HttpRequest):
-    list_items = ""
-    
     months = list(MONTHLY_CHALLENGES.keys())
     
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-        
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
+    
 
 def monthly_challenge_by_number(request: HttpRequest, month: int):
     try:
@@ -43,6 +38,9 @@ def monthly_challenge_by_number(request: HttpRequest, month: int):
 def monthly_challenge(request: HttpRequest, month: str):
     try:
         challenge_text = MONTHLY_CHALLENGES[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month": month
+        })
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
